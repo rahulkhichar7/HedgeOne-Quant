41884f6c37f950fb2cfbd0a43e316368b2880dfb
Srajan Dehariya <dehariyasrajan@gmail.com>
2025-12-06 17:49:24 +0530

    Made chart resizable, added React interface for showing charts; TODO: Added tooltip and color with it, used useState for saving the state of chart i.e. different stocks, indicators, strategies data

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,7 +1,6 @@
 'use client'
 
-import { useEffect, useRef } from "react";
-import { AreaSeries, CandlestickSeries, createChart } from 'lightweight-charts'
+import { useEffect, useId, useRef, useState } from "react";
 import { Autocomplete, AutocompleteEmpty, AutocompleteInput, AutocompleteItem, AutocompleteList, AutocompletePopup, AutocompletePositioner } from "@/components/ui/autocomplete";
 import { Button } from "@/components/ui/button";
 import {
@@ -9,34 +8,10 @@ import {
   ResizablePanel,
   ResizablePanelGroup,
 } from "@/components/ui/resizable";
+import { Chart, ChartData} from "@/components/chart";
+import { CandlestickData } from "lightweight-charts";
 
-const initialData = [
-  { time: '2018-12-22', value: 32.51 },
-  { time: '2018-12-23', value: 31.11 },
-  { time: '2018-12-24', value: 27.02 },
-  { time: '2018-12-25', value: 27.32 },
-  { time: '2018-12-26', value: 25.17 },
-  { time: '2018-12-27', value: 28.89 },
-  { time: '2018-12-28', value: 25.46 },
-  { time: '2018-12-29', value: 23.92 },
-  { time: '2018-12-30', value: 22.68 },
-  { time: '2018-12-31', value: 22.67 },
-];
 
-const ohclData = [
-  { time: '2018-12-22', open: 75.16, high: 82.84, low: 36.16, close: 45.72 },
-  { time: '2018-12-23', open: 45.12, high: 53.90, low: 45.12, close: 48.09 },
-  { time: '2018-12-24', open: 60.71, high: 60.71, low: 53.39, close: 59.29 },
-  { time: '2018-12-25', open: 68.26, high: 68.26, low: 59.04, close: 60.50 },
-  { time: '2018-12-26', open: 67.71, high: 105.85, low: 66.67, close: 91.04 },
-  { time: '2018-12-27', open: 91.04, high: 121.40, low: 82.70, close: 111.40 },
-  { time: '2018-12-28', open: 111.51, high: 142.83, low: 103.34, close: 131.25 },
-  { time: '2018-12-29', open: 131.33, high: 151.17, low: 77.68, close: 96.43 },
-  { time: '2018-12-30', open: 106.33, high: 110.20, low: 90.39, close: 98.10 },
-  { time: '2018-12-31', open: 109.87, high: 114.69, low: 85.66, close: 111.26 },
-]
-
-const chartOptions = { layout: { textColor: 'black', background: { type: 'solid', color: 'white' } } };
 const tags: Tag[] = [
   { id: "t1", value: "feature" },
   { id: "t2", value: "fix" },
@@ -48,91 +23,137 @@ const tags: Tag[] = [
   { id: "c-alert-dialog", value: "component: alert dialog" },
   { id: "c-autocomplete", value: "component: autocomplete" },
   { id: "c-avatar", value: "component: avatar" },
-  { id: "c-checkbox", value: "component: checkbox" },
-  { id: "c-checkbox-group", value: "component: checkbox group" },
-  { id: "c-collapsible", value: "component: collapsible" },
-  { id: "c-combobox", value: "component: combobox" },
-  { id: "c-context-menu", value: "component: context menu" },
-  { id: "c-dialog", value: "component: dialog" },
-  { id: "c-field", value: "component: field" },
-  { id: "c-fieldset", value: "component: fieldset" },
-  { id: "c-filterable-menu", value: "component: filterable menu" },
-  { id: "c-form", value: "component: form" },
-  { id: "c-input", value: "component: input" },
-  { id: "c-menu", value: "component: menu" },
-  { id: "c-menubar", value: "component: menubar" },
-  { id: "c-meter", value: "component: meter" },
-  { id: "c-navigation-menu", value: "component: navigation menu" },
-  { id: "c-number-field", value: "component: number field" },
-  { id: "c-popover", value: "component: popover" },
-  { id: "c-preview-card", value: "component: preview card" },
-  { id: "c-progress", value: "component: progress" },
-  { id: "c-radio", value: "component: radio" },
-  { id: "c-scroll-area", value: "component: scroll area" },
-  { id: "c-select", value: "component: select" },
-  { id: "c-separator", value: "component: separator" },
-  { id: "c-slider", value: "component: slider" },
-  { id: "c-switch", value: "component: switch" },
-  { id: "c-tabs", value: "component: tabs" },
-  { id: "c-toast", value: "component: toast" },
-  { id: "c-toggle", value: "component: toggle" },
-  { id: "c-toggle-group", value: "component: toggle group" },
-  { id: "c-toolbar", value: "component: toolbar" },
   { id: "c-tooltip", value: "component: tooltip" },
 ];
-export default function Home() {
-  useEffect(() => {
-    const chart = createChart(document.getElementById('chart-container') ?? "", chartOptions);
-    const areaSeries = chart.addSeries(AreaSeries, {
-      lineColor: '#2962FF', topColor: '#2962FF',
-      bottomColor: 'rgba(41, 98, 255, 0.28)',
-    });
-    areaSeries.setData(initialData)
+const initialData = [
+    { time: '2018-12-22', value: 32.51 },
+    { time: '2018-12-23', value: 31.11 },
+    { time: '2018-12-24', value: 27.02 },
+    { time: '2018-12-25', value: 27.32 },
+    { time: '2018-12-26', value: 25.17 },
+    { time: '2018-12-27', value: 28.89 },
+    { time: '2018-12-28', value: 25.46 },
+    { time: '2018-12-29', value: 23.92 },
+    { time: '2018-12-30', value: 22.68 },
+    { time: '2018-12-31', value: 22.67 },
+];
 
-    const candlestickSeries = chart.addSeries(CandlestickSeries, {
-      upColor: '#26a69a', downColor: '#ef5350', borderVisible: false,
-      wickUpColor: '#26a69a', wickDownColor: '#ef5350',
-    });
-    candlestickSeries.setData(ohclData)
-    chart.timeScale().fitContent();
-  });
+const chartOptions = { layout: { textColor: 'black', background: { type: 'solid', color: 'white' } } };
+const ohclData = [
+    { time: '2018-12-22', open: 75.16, high: 82.84, low: 36.16, close: 45.72 },
+    { time: '2018-12-23', open: 45.12, high: 53.90, low: 45.12, close: 48.09 },
+    { time: '2018-12-24', open: 60.71, high: 60.71, low: 53.39, close: 59.29 },
+    { time: '2018-12-25', open: 68.26, high: 68.26, low: 59.04, close: 60.50 },
+    { time: '2018-12-26', open: 67.71, high: 105.85, low: 66.67, close: 91.04 },
+    { time: '2018-12-27', open: 91.04, high: 121.40, low: 82.70, close: 111.40 },
+    { time: '2018-12-28', open: 111.51, high: 142.83, low: 103.34, close: 131.25 },
+    { time: '2018-12-29', open: 131.33, high: 151.17, low: 77.68, close: 96.43 },
+    { time: '2018-12-30', open: 106.33, high: 110.20, low: 90.39, close: 98.10 },
+    { time: '2018-12-31', open: 109.87, high: 114.69, low: 85.66, close: 111.26 },
+]
+
+const data = [
+    {
+        label: "Series1",
+        color: "blue",
+        type: "LineSeries",
+        data: initialData,
+        other: ""
+    },
+    {
+        label: "Series2",
+        color: "black",
+        type: "CandlesticksSeries",
+        data: ohclData,
+        other: ""
+    }
+]
+
+// Fetch stock/index data here
+function fetchData(name: string){
+  return []
+}
+
+const default_state = {
+  stocks: [
+    {
+      label: "Name",
+      color: "blue",
+      type:  "CandlesticksSeries",
+      other:  "where did you apply this",
+      data:  ohclData
+    }
+  ],
+  indicators: [
+    {
+      label:  "Name",
+      color:  "blue",
+      other:     "where did you apply this",
+      type:   "LineSeries",
+      data:   initialData
+    }
+  ],
+  strategies: [
+    {
+      label:  "Name",
+      color:  "blue",
+      other:     "where did you apply this",
+      type:   "LineSeries", 
+      data:   initialData
+    }
+  ]
+}
+
+export default function Home() {
+  // const [state, setState] = useState({
+  //   stocks: [] as ChartData[],
+  //   indicators: [] as ChartData[],
+  //   strategies: [] as ChartData[]
+  // })
+  const [state, setState] = useState(default_state)
+
   return (
     <div className="items-center bg-zinc-50 font-sans dark:bg-black">
       <div className="flex flex-row justify-left gap-2 m-1">
-        <Autocomplete items={tags}>
-          <AutocompleteInput
-            id="tags"
-            placeholder="Stock/Index Name"
-            className="w-xs"
-          />
-          <AutocompletePositioner sideOffset={6}>
-            <AutocompletePopup>
-              <AutocompleteEmpty>No tags found.</AutocompleteEmpty>
-              <AutocompleteList>
-                {(tag) => (
-                  <AutocompleteItem key={tag.id} value={tag.value}>
-                    {tag.value}
-                  </AutocompleteItem>
-                )}
-              </AutocompleteList>
-            </AutocompletePopup>
-          </AutocompletePositioner>
-        </Autocomplete>
-        <Button>Backtest</Button>
+          <Button>Backtest</Button>
       </div>
       <ResizablePanelGroup
         direction="horizontal"
         className="h-full"
       >
         <ResizablePanel defaultSize={25}>
-          <div className="flex h-full items-center justify-center">
-            <span className="font-semibold">Sidebar</span>
+          <div className="h-full justify-left p-2">
+            <div className="border bg-amber-50 p-2">
+            <p>Stocks/Indices <Button>Add</Button></p>
+              <ul>
+                {state.stocks.map(stock =>
+                  <li key={useId()}>{stock.label}</li>
+                )}
+              </ul>
+            </div>
+            <div className="border bg-amber-50 p-2">
+              <p>Indicators <Button>Add</Button></p>
+              <ul>
+                {state.indicators.map(indicator =>
+                  <li key={useId()}>{indicator.label}</li>
+                )}
+              </ul>
+            </div>
+            <div className="border bg-amber-50 p-2">
+              <p>Strategies <Button>Add</Button></p>
+              <ul>
+                {state.strategies.map(strategy =>
+                  <li key={useId()}>{strategy.label}</li>
+                )}
+              </ul>
+            </div>
+            {/* <span className="font-semibold">Sidebar</span> */}
           </div>
         </ResizablePanel>
         <ResizableHandle withHandle />
         <ResizablePanel defaultSize={75}>
           <div className="flex h-full items-center justify-center">
-            <div id="chart-container" className="w-screen h-[calc(100vh-theme(space.16))]"></div>
+            <Chart data={[...state.stocks, ...state.indicators, ...state.strategies]}></Chart>
           </div>
         </ResizablePanel>
       </ResizablePanelGroup>
